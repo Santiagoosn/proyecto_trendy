236e018b8b1cdb254df2dd77263af2bbfaa2ec66
Santiagoosn <127985624+Santiagoosn@users.noreply.github.com>
2023-09-03 20:28:25 -0500

    Add files via upload

--- app_flask/index.py
+++ app_flask/index.py
@@ -14,6 +14,18 @@ def catalogo():
 def login():
     return render_template ('login.html')
 
+@app.route('/nosotros')
+def nosotros():
+    return render_template ('nosotros.html')
+
+@app.route('/servicios')
+def servicios():
+    return render_template ('servicios.html')
+
+@app.route('/registro')
+def registro():
+    return render_template ('registro.html')
+
 
 
 if __name__=='__main__':
